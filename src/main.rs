3a1819563809485e40039b5ae6830d6e9ae20d6b
Jacob McSwain <jacob@mcswain.dev>
2026-03-30 22:25:03 -0500

    fix default logging

--- src/main.rs
+++ src/main.rs
@@ -44,7 +44,7 @@ struct Cli {
 }
 
 fn main() {
-    env_logger::init();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     let cli = Cli::parse();
 
     log::info!(
